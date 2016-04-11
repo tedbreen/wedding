07066da4931b596f7e6009ea1ee57787d08b174c
Edward Breen <ted.breen@gmail.com>
2016-04-10 17:51:41 -0700

    move banner images to header partial

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,23 @@ var details = require('../data/details.json')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Katie & Ted'})
+  res.render('index', {
+    title: 'Katie & Ted'
+  , headerClass: 'index-main-image'
+  })
 })
 
 router.get('/details', function (req, res, next) {
-  res.render('details', {details: details})
+  res.render('details', {
+    details: details
+  , headerClass: 'details-main-image'
+  })
 })
 
 router.get('/lodging', function (req, res, next) {
-  res.render('lodging')
+  res.render('lodging', {
+    headerClass: 'lodging-main-image'
+  })
 })
 
 module.exports = router

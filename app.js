5e331315aa682c628dc5de331c82af09c6c753f5
Edward Breen <ted.breen@gmail.com>
2016-04-11 19:28:15 -0700

    server: use gzip compression

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var hbs = require('hbs')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+var compression = require('compression')
 
 var routes = require('./routes/')
 
@@ -20,6 +21,7 @@ hbs.registerPartials(__dirname + '/views/partials')
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(compression())
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
